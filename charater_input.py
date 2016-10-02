2526ec6d07af48df75b9f2cb479c2187d132d172
chandni vaya <chandni@chandnis-MacBook-Air.local>
2016-10-02 19:30:38 +0400

    try

---
+++ charater_input.py
@@ -0,0 +1,7 @@
+name = raw_input("what is your name? ")
+age = int(input("what is your age? "))
+times = int(input("how many times? "))
+year = 2016+(100 - age)
+print (times*("hi %s you will be 100 in the year %r \n" %(name,year)))
+
+
